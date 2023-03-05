fc7655074b977334c295b506add9b1d5ffc79e28
shahin <shahin.ccie1989@gmail.com>
2023-03-05 09:49:43 +0100

    clean up

--- Misc/PdfToText.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "../src/components/Layout";
-
-export default function PdfToText() {
-  return (
-    <Layout headerOne={false} headerTwo="PdfToText">
-      <div>PdfToText</div>
-    </Layout>
-  );
-}
